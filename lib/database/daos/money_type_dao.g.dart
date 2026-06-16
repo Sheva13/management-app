f5b0ca15a0dfe180a76cf87a901ae658abc13cb3
Sheva13 <farrelsheva13@gmail.com>
2026-06-16 23:22:36 +0700

    Update .gitignore: remove generated files and build artifacts

--- lib/database/daos/money_type_dao.g.dart
+++
@@ -1,8 +0,0 @@
-// GENERATED CODE - DO NOT MODIFY BY HAND
-
-part of 'money_type_dao.dart';
-
-// ignore_for_file: type=lint
-mixin _$MoneyTypeDaoMixin on DatabaseAccessor<AppDatabase> {
-  $MoneyTypesTableTable get moneyTypesTable => attachedDatabase.moneyTypesTable;
-}
--- lib/database/daos/platform_dao.g.dart
+++
@@ -1,9 +0,0 @@
-// GENERATED CODE - DO NOT MODIFY BY HAND
-
-part of 'platform_dao.dart';
-
-// ignore_for_file: type=lint
-mixin _$PlatformDaoMixin on DatabaseAccessor<AppDatabase> {
-  $MoneyTypesTableTable get moneyTypesTable => attachedDatabase.moneyTypesTable;
-  $PlatformsTableTable get platformsTable => attachedDatabase.platformsTable;
-}
